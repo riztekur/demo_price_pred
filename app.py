da49cf39a692573dab5d7831cd0e0606a277b127
riztekur <riztekur@gmail.com>
2023-11-21 02:05:40 +0700

    new model

--- app.py
+++ app.py
@@ -1,47 +1,49 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import numpy as np
+from datetime import datetime
 
-def create_time_feature(df):
-    df['date'] = pd.to_datetime(df['date'])
-    df['dayofmonth'] = df['date'].dt.day
-    df['dayofweek'] = df['date'].dt.dayofweek
-    df['quarter'] = df['date'].dt.quarter
-    df['month'] = df['date'].dt.month
-    df['year'] = df['date'].dt.year
-    df['dayofyear'] = df['date'].dt.dayofyear
-    df.drop(columns=['date'], inplace=True)
-    return df
+df = pd.read_csv('prep_data.csv', parse_dates=['date'])
+result = pd.read_csv('result.csv', parse_dates=['date'])
 
-df = pd.read_csv('df_prep.csv')
-model = pickle.load(open('model.sav','rb'))
+df['date'] = df['date'].dt.date
+result['date'] = result['date'].dt.date
 
-with st.form('input_form'):
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        country = st.selectbox('Country', options=df['country'].unique())
-    with col2:
-        area = st.selectbox('Area', options=df['area'].unique())
-    with col3:
-        size = st.selectbox('Size', options=df['size'].unique())
-    with col4:
-        date = st.date_input('Date')
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    country = st.selectbox('Country', options=result['country'].unique())
+with col2:
+    area = st.selectbox('Area', options=result[result['country']==country]['area'].unique())
+with col3:
+    size = st.selectbox('Size', options=result[(result['country']==country) & (result['area']==area)]['size'].unique())
+with col4:
+    date = st.date_input('Date')
 
-    submitted = st.form_submit_button("Predict")
+res1, res2 = st.columns(2)
+with res1:
+    st.subheader("Result")
+with res2:
+    to_usd = st.toggle('Show in USD')
 
-if submitted:
-    x = pd.DataFrame(
-        [[country,area,size,date]],
-        columns=['country','area','size','date']
-    )
+currency = {
+    "INDONESIA" : "IDR",
+    "CHINA" : "CNY",
+    "INDIA" : "INR",
+    "ECUADOR" : "USD",
+    "THAILAND" : "THB",
+    "VIETNAM" : "VND"
+}
 
-    x['country'] = pd.Categorical(x['country'], categories=df['country'].unique())
-    x['area'] = pd.Categorical(x['area'], categories=df['area'].unique())
-    x['size'] = pd.Categorical(x['size'], categories=df['size'].unique())
+if to_usd:
+    price = 'price_usd'
+    sign = "USD"
+else:
+    price = 'price_origin'
+    sign = currency[country]
+pred = result.loc[
+    (result['country'] == country) 
+    & (result['area'] == area)
+    & (result['size'] == size)
+    & (result['date'] == date), [price]].values[0][0]
 
-    st.dataframe(x, use_container_width=True, hide_index=True)
-
-    x = create_time_feature(x)
-
-    pred = model.predict(x)[0]
-    st.metric("Price", f'Rp{pred:,.2f}')
+st.subheader(f'{sign} {pred:,.2f}')
